7fd4274a7e8337f040596ca9c93b962dc6d5243c
bayers92 <brianc.ayers@gmail.com>
2016-02-01 17:50:09 -0500

    future workout page

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,3 +1,5 @@
 Time::DATE_FORMATS[:my_long_ordinal] = lambda { |time| time.strftime("%A - %b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y") }
 
 Time::DATE_FORMATS[:my_short_ordinal] = lambda { |time| time.strftime("%b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y") }
+
+Time::DATE_FORMATS[:day_of_week] = lambda { |time| time.strftime("%A (%b #{ActiveSupport::Inflector.ordinalize(time.day)})") }
