de5ed5e1716f5ddb7f53b9a65db76bf7cee951cc
bayers92 <brianc.ayers@gmail.com>
2016-03-18 11:11:51 -0400

    short day of week

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,4 @@
-Time::DATE_FORMATS[:my_long_ordinal] = lambda { |time| time.strftime("%A - %b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y") }
+Time::DATE_FORMATS[:my_long_ordinal] = lambda { |time| time.strftime("%a - %b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y") }
 
 Time::DATE_FORMATS[:my_short_ordinal] = lambda { |time| time.strftime("%b #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y") }
 
