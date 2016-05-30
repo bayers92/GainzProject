95808c8f9a5781e0d88bfa74a3b9f1da916a9e2b
bayers92 <brianc.ayers@gmail.com>
2016-05-30 11:20:07 -0400

    production mailer?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'gainzproject.com' }
-  Rails.application.routes.default_url_options[:host] = 'gainzproject.com'
+  config.action_mailer.default_url_options = { :host => 'frozen-gorge-9009.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
@@ -91,6 +90,6 @@ Rails.application.configure do
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+  password: ENV["GMAIL_PASSWORD"],
   }
 end
